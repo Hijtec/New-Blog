11742813214eabda542f12778a21f4de2584fb64
Hijtec <m.cernil@gmail.com>
2017-03-28 21:10:50 +0200

    preparing for contact form

--- app/forms.py
+++ app/forms.py
@@ -4,6 +4,6 @@ from wtforms import validators, ValidationError
 
 class ContactForm(FlaskForm):
     name=TextField("Your name");
-    email=TextField("Email",[validators.Required("Please enter your email adress."), validators.Email("Please enter proper email adress")])
+    email=TextField("Email",[validators.Required("Please enter your email adress."), validators.Email("Please enter proper email adress")]);
     message=TextAreaField("Your message");
     submit=SubmitField("Send")
